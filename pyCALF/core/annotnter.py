54f6855dbbe65c568611c040e56657f9d2c36c15
Millet Maxime <maxime.millet@etu.univ-nantes.fr>
2022-06-24 09:08:47 +0200

    .

--- pyCALF/core/annotnter.py
+++ pyCALF/core/annotnter.py
@@ -2,8 +2,43 @@ import os
 import logging
 import subprocess
 import pandas as pd
-
+import shutil
+import sys
 from ..utils import utils as u
+
+def run(cmd):
+    """Run the given command line string via subprocess."""
+    # Avoid using shell=True when we call subprocess to ensure if the Python
+    # script is killed, so too is the child process.
+    try:
+        child = subprocess.Popen(
+            cmd, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+    except Exception as err:
+        sys.exit("Error invoking command:\n%s\n\n%s\n" % (" ".join(cmd), err))
+    stdout, stderr = child.communicate()
+    return_code = child.returncode
+
+    # keep stdout minimal as shown prominently in Galaxy
+    # Record it in case a silent error needs diagnosis
+    if stdout:
+        sys.stderr.write("Standard out:\n%s\n\n" % stdout)
+    if stderr:
+        sys.stderr.write("Standard error:\n%s\n\n" % stderr)
+
+    error_msg = None
+    if return_code:
+        cmd_str = " ".join(cmd)
+        error_msg = "Return code %i from command:\n%s" % (return_code, cmd_str)
+    elif "Database or network connection (timeout) error" in stdout + stderr:
+        error_msg = "Database or network connection (timeout) error"
+    elif "Annotation of 0 seqs with 0 annots finished." in stdout + stderr:
+        error_msg = "No sequences processed!"
+
+    if error_msg:
+        print(error_msg)
+        sys.exit(error_msg)
+
 
 def blastp(query,subject,evalue = 1e-4):
     import sys
@@ -11,16 +46,21 @@ def blastp(query,subject,evalue = 1e-4):
         from StringIO import StringIO
     else:
         from io import StringIO
-    o = subprocess.run([
+
+
+    command = [
             "blastp",
             "-outfmt" , 
             "6 delim=; qacc sacc qlen slen evalue bitscore score pident nident mismatch qstart qend sstart send length qseq sseq qcovs qcovhsp", 
             "-query" , query,
             "-subject", subject,
             "-evalue" , str(evalue),
-        ], capture_output=True)
-    
+        ]
+        
+    o = subprocess.run(command,capture_output=True)
+    print(o)
     res = o.stdout.decode('ascii').strip()
+    print(res)
     if res:
         df = pd.read_table( StringIO(res)  , sep=";"  , header=None )          
         df.columns = "qacc sacc qlen slen evalue bitscore score pident nident mismatch qstart qend sstart send length qseq sseq qcovs qcovhsp".split(" ")
